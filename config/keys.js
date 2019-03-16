3c2e771ebbfdefeb270b9422b86a31f84ebf4bd7
Rohit Chhetri <rohit.chhetri@gmail.com>
2019-03-15 22:35:17 -0700

    update key

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
-    MongoURI:'mongodb://localhost:27017/mantabya'
+    MongoURI:'mongodb+srv://projectdb:Project19@mongotestcluster-3d8gd.mongodb.net/test?retryWrites=true'
+
 }
 
 //mongodb+srv://projectdb:Project19@mongotestcluster-3d8gd.mongodb.net/test?retryWrites=true'
